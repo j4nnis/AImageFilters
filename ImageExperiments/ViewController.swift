b95bc0307b208b69eafe37889d957ba982b20639
Jannis Muething <muething@smartcircles.de>
2014-10-02 12:25:55 +0200

    Initial Commit

---
+++ ImageExperiments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageExperiments
+//
+//  Created by Jannis Muething on 10/2/14.
+//  Copyright (c) 2014 Jannis Muething. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
